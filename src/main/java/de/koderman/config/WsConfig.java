43adcc9d85c192849fbabf22f3014d03f8d26ab5
Alexander Koderman <github@koderman.de>
2025-11-17 22:52:05 +0000

    Fix chair disconnects

--- src/main/java/de/koderman/config/WsConfig.java
+++ src/main/java/de/koderman/config/WsConfig.java
@@ -1,8 +1,11 @@
 package de.koderman.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -17,7 +20,19 @@ class WsConfig implements WebSocketMessageBrokerConfigurer {
     }
     @Override
     public void configureMessageBroker(@NonNull MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic");
+        // Enable simple broker with 5-second heartbeat: [server -> client ms, client -> server ms]
+        registry.enableSimpleBroker("/topic")
+                .setHeartbeatValue(new long[] {5000L, 5000L}) // 5s keepalive
+                .setTaskScheduler(heartbeatTaskScheduler()); // Required for heartbeats
         registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Bean
+    public TaskScheduler heartbeatTaskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(1);
+        scheduler.setThreadNamePrefix("websocket-heartbeat-");
+        scheduler.initialize();
+        return scheduler;
     }
 }
