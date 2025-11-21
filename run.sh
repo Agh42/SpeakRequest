aa77452cbea6d0889847a5b28f9ef0a18b562217
Alexander Koderman <github@koderman.de>
2025-11-21 14:55:39 +0100

    restart until stopped

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d --name speakrequest -p 8080:8080 agh42/speakrequest:latest
+docker run -d --name speakrequest --restart unless-stopped -p 8080:8080 agh42/speakrequest:latest
